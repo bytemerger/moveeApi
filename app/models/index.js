09e011778bf6cdb3ff3d4fbc89829ee8f46a947a
franc <onyishif@gmail.com>
2021-11-15 08:28:19 +0100

    more semantics on the model

--- app/models/index.js
+++ app/models/index.js
@@ -2,8 +2,8 @@ const config = require('../../config/secret');
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(config.MYSQL_DATABASE_URL);
 
+require('./comment')(sequelize);
 (async () => {
     await sequelize.sync({ force: true });
 })();
-require('./comment')(sequelize);
 module.exports = sequelize
