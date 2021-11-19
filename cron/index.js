b876bcf6c7cd4372170ba98aa4ae16d05a9ddb56
franc <onyishif@gmail.com>
2021-11-19 13:21:13 +0100

    refactor to get all db connections from config/db complete docker setup

--- cron/index.js
+++ cron/index.js
@@ -1,9 +1,5 @@
-const redis = require('redis');
-const promisify = require('util').promisify;
 const { fetchLoop } = require('../app/helpers/index');
 const {setAsync} = require('../config/db');
-
-const client = redis.createClient();
 
 async function getData(){
     const characters = await fetchLoop('https://swapi.dev/api/people/');
